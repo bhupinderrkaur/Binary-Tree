72d44db2f3462768c25ed7e5e3627bdbdad6cffd
Bhupinder Kaur <99190152+bhupinderrkaur@users.noreply.github.com>
2022-03-23 17:20:31 +0530

    Update preOrderTraversal.java

--- preOrderTraversal.java
+++ preOrderTraversal.java
@@ -17,3 +17,38 @@ class BTree
         // Traverse the right subtree
         preorder(root.right);
     }
+   
+   
+   
+   //iterative
+    public static void preorderIterative(Node root)
+    {
+      
+        if (root == null) {
+            return;
+        }
+    
+      
+        Stack<Node> stack = new Stack<>();
+        stack.push(root);
+    
+        while (!stack.empty())
+        {
+            Node curr = stack.pop();
+    
+            System.out.print(curr.data + " ");
+    
+            
+            if (curr.right != null) {
+                stack.push(curr.right);
+            }
+    
+           
+            if (curr.left != null) {
+                stack.push(curr.left);
+            }
+    
+            // the right child must be pushed first so that the left child
+            
+        }
+    }
