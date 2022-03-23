b289cab03af02a204042119e600383a0667a1a0d
Bhupinder Kaur <99190152+bhupinderrkaur@users.noreply.github.com>
2022-03-23 17:17:59 +0530

    Update inorderTraversal.java

--- inorderTraversal.java
+++ inorderTraversal.java
@@ -16,3 +16,36 @@ class Tree
         // Traverse the right subtree
         inorder(root.right);
     }
+        
+    
+    
+    //iterative method
+ public static void inorderIterative(Node root)
+    {
+       
+        Stack<Node> stack = new Stack<>();
+    
+       
+        Node curr = root;
+    
+ 
+        while (!stack.empty() || curr != null)
+        {
+           
+            if (curr != null)
+            {
+                stack.push(curr);
+                curr = curr.left;
+            }
+            else {
+                // otherwise, if the current node is null, pop an element from
+                // the stack, print it, and finally set the current node to its
+                // right child
+                curr = stack.pop();
+                System.out.print(curr.data + " ");
+    
+                curr = curr.right;
+            }
+        }
+    }
+ 
