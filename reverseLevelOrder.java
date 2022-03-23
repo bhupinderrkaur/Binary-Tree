37c186d3e1d0d6a0fec821abb17913dbb27db5cb
Bhupinder Kaur <99190152+bhupinderrkaur@users.noreply.github.com>
2022-03-23 16:19:11 +0530

    Create reverseLevelOrder.java

---
+++ reverseLevelOrder.java
@@ -0,0 +1,40 @@
+class Node
+{
+    int data;
+    Node left, right;
+
+    Node(int item)
+    {
+        data = item;
+        left = right = null;
+    }
+}
+
+
+class Tree
+{
+    public ArrayList<Integer> reverseLevelOrder(Node node) 
+    {
+        // code here
+        ArrayList<Integer> a=new ArrayList<Integer>();
+        Stack<Node> st=new Stack<>();
+        Queue<Node> q = new LinkedList<>();
+         q.add(node);
+        while(q.size()>0){
+            
+            node=q.peek();
+            q.remove();
+            st.add(node);
+            
+            if(node.right!=null)
+            q.add(node.right);
+            if(node.left!=null)
+            q.add(node.left);
+        }
+        while(!st.isEmpty()){
+            Node t=st.pop();
+            a.add(t.data);
+        }
+        return a;
+    }
+}      
